5f97b3521b76a5f4734e4911d74c0fb9f4a064e3
Scott Silliman <scott.t.silliman@gmail.com>
2020-05-01 22:27:16 -0400

    now makes use of config/eleventy-config

--- apps/di-prepost/.eleventy.js
+++ apps/di-prepost/.eleventy.js
@@ -1,68 +1,31 @@
-const prettier = require("prettier");
-const uuidV1 = require("uuid/v1");
+const fs = require("fs");
+const path = require("path");
 
-// both indents html output and catches some html syntax errors
-function prettify(content, outputPath) {
-    if (outputPath && outputPath.endsWith(".html")) {
-        return prettier.format(content, { parser: "html" })
-    }
-    return content;
-}
-
-function uuid(prefix) {
-    return `${prefix}-${uuidV1()}`
-}
-
-function verticallyCenter(content, pctFromTop = 30) {
-    return `
-        <div class="dev centered-wrapper">
-            <div class="dev centered" style="top:${pctFromTop}%;">
-                ${content}
-            </div>
-        </div>
-    `;
-}
+const {
+    prettify,
+    uuid,
+    verticallyCenter,
+    jsonify,
+    mergeScenesWithData
+} = require("@isptutorproject/eleventy-config");
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addTransform("prettier", prettify);
-
-    eleventyConfig.addWatchTarget("./dist/");
-    eleventyConfig.addWatchTarget("./src/js/");
-    eleventyConfig.addWatchTarget("./src/css/");
-
-    eleventyConfig.addPassthroughCopy({ "dist": "." });
-    eleventyConfig.addPassthroughCopy({ "src/img": "img" });
-
-    
-    eleventyConfig.addNunjucksFilter("jsonify", function (obj, indent = null) {
-        return JSON.stringify(obj, null, indent);
-    });
-
+    eleventyConfig.addNunjucksFilter("jsonify", jsonify);
     eleventyConfig.addNunjucksFilter("uuid", uuid);
-
     eleventyConfig.addPairedShortcode("vertcenter", verticallyCenter);
-
-    eleventyConfig.addCollection("diPretestScenes", function (collection) {
-        let scenes = collection.getFilteredByTag("pretestScenes");
-        let sceneData = collection.getAll()[0].data.pretest.scenes;
-
-        let munged = scenes.map((item) => {
-            let id = item.data.page.fileSlug;
-            // console.log(id);
-            let frontMatter = item.template.frontMatter.data;
-            // console.log("\tfrontMatter", frontMatter);
-            // console.log("\tsceneData", sceneData[id]);
-            item.data.page.data = Object.assign({}, frontMatter, sceneData[id]);
-            return item;
-        });
-        return munged;
+    
+    eleventyConfig.addCollection("diPretestScenesWithData", function (collection) {
+        let scenes = collection.getFilteredByTag("diPreTestScenes");
+        let sceneData = collection.getAll()[0].data.diPreTest.scenes;
+        return mergeScenesWithData(scenes, sceneData);
     });
 
     return {
         dir: {
-            input: "src/templates",
+            input: "templates",
             includes: "_includes",
-            data: "_data"
+            output: "dist"
         },
         templateFormats: ["njk"]
     }
