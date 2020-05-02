e048d1b993885245b44bc3a49e721982661f697d
Scott Silliman <scott.t.silliman@gmail.com>
2020-05-01 23:10:48 -0400

    change path to data file

--- apps/di-instruction/src/index.js
+++ apps/di-instruction/src/index.js
@@ -3,7 +3,7 @@ import { DiInstructionApp } from "./di-instruction-app";
 
 import "./styles/index.css";
 
-let appData = require("@isptutorproject/isp-data/dist/diInstr.json");
+let appData = require("../data/diInstr.json");
 
 let app = new DiInstructionApp(appData)
 app.setStartScene("scene1");
