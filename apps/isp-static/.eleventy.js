ea4c0aef182f6ad54b5828c966e4f73199636c90
Scott Silliman <scott.t.silliman@gmail.com>
2020-05-01 23:14:16 -0400

    now simply copies di-instruction and di-pretest rather than generating them

--- apps/isp-static/.eleventy.js
+++ apps/isp-static/.eleventy.js
@@ -11,17 +11,22 @@ const {
 
 const APPS = {
     "homepage": ".",
-    "di-instruction": "di-instruction",
-    "di-prepost": "di-prepost",
     "hypo-defs": "hypo/defs"
 };
 
+// "di-instruction": "di-instruction",
+// "di-prepost": "di-prepost",
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addTransform("prettier", prettify);
     eleventyConfig.addNunjucksFilter("jsonify", jsonify);
     eleventyConfig.addNunjucksFilter("uuid", uuid);
     eleventyConfig.addPairedShortcode("vertcenter", verticallyCenter);
+
+    eleventyConfig.addWatchTarget('../di-instruction/dist/*.*');
+    eleventyConfig.addPassthroughCopy({"../di-instruction/dist": "di-instruction"});
+    eleventyConfig.addWatchTarget('../di-prepost/dist/*.*');
+    eleventyConfig.addPassthroughCopy({ "../di-prepost/dist": "di-prepost" });
 
     for (let [app, appPath] of Object.entries(APPS)) {
         eleventyConfig.addWatchTarget(`../${app}/dist/*.*`);
@@ -31,17 +36,17 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy({"img": "img"});
     // eleventyConfig.addPassthroughCopy({"src/styles/*.*": "."});
 
-    eleventyConfig.addCollection("diInstrScenesWithData", function (collection) {
-        let scenes = collection.getFilteredByTag("diInstrScenes");
-        let sceneData = collection.getAll()[0].data.diInstr.scenes;
-        return mergeScenesWithData(scenes, sceneData);
-    });
+    // eleventyConfig.addCollection("diInstrScenesWithData", function (collection) {
+    //     let scenes = collection.getFilteredByTag("diInstrScenes");
+    //     let sceneData = collection.getAll()[0].data.diInstr.scenes;
+    //     return mergeScenesWithData(scenes, sceneData);
+    // });
 
-    eleventyConfig.addCollection("diPreTestScenesWithData", function (collection) {
-        let scenes = collection.getFilteredByTag("diPreTestScenes");
-        let sceneData = collection.getAll()[0].data.diPreTest.scenes;
-        return mergeScenesWithData(scenes, sceneData);
-    });
+    // eleventyConfig.addCollection("diPreTestScenesWithData", function (collection) {
+    //     let scenes = collection.getFilteredByTag("diPreTestScenes");
+    //     let sceneData = collection.getAll()[0].data.diPreTest.scenes;
+    //     return mergeScenesWithData(scenes, sceneData);
+    // });
 
     eleventyConfig.addCollection("hypoDefScenesWithData", function (collection) {
         let scenes = collection.getFilteredByTag("hypoDefScenes");
