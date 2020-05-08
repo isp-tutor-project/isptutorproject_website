347c35f89ad1b951779a172b43403905166c2839
Scott Silliman <scott.t.silliman@gmail.com>
2020-05-08 15:35:00 -0400

    changed di-instr/dist/*.* to di-instr/dist/ so images get copied

--- apps/isp-static/.eleventy.js
+++ apps/isp-static/.eleventy.js
@@ -1,7 +1,7 @@
 // this config simply copies all of the individually built apps into wwwroot
 
 const APPS = {
-    "../di-instr/dist/*.*": "di-instr",
+    "../di-instr/dist/": "di-instr",
     "../di-prepost/dist/*.*": "di-prepost",
     "../homepage/dist/*.*": ".",
     "../hypo/dist/*.*": "hypo"
