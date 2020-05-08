e0da431b3dafa3571b888e3faf43b959bbc45d20
Scott Silliman <scott.t.silliman@gmail.com>
2020-05-08 15:32:57 -0400

    removed kruft and added passThruCopy of images

--- apps/di-instr/.eleventy.js
+++ apps/di-instr/.eleventy.js
@@ -9,10 +9,6 @@ const {
     mergeScenesWithData
 } = require("@isptutorproject/eleventy-config");
 
-const APPS = {
-    "di-instruction": "di-instruction",
-};
-
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addTransform("prettier", prettify);
@@ -20,36 +16,17 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addNunjucksFilter("uuid", uuid);
     eleventyConfig.addPairedShortcode("vertcenter", verticallyCenter);
 
-    // for (let [app, appPath] of Object.entries(APPS)) {
-    //     eleventyConfig.addWatchTarget(`../${app}/dist/*.*`);
-    //     eleventyConfig.addPassthroughCopy({[`../${app}/dist/*.*`]: appPath});
-    // }
+    eleventyConfig.addPassthroughCopy("templates/img");
     
-    // eleventyConfig.addPassthroughCopy({"img": "img"});
-    // eleventyConfig.addPassthroughCopy({"src/styles/*.*": "."});
-
     eleventyConfig.addCollection("diInstrScenesWithData", function (collection) {
         let scenes = collection.getFilteredByTag("diInstrScenes");
         let sceneData = collection.getAll()[0].data.diInstr.scenes;
         return mergeScenesWithData(scenes, sceneData);
     });
-
-    // eleventyConfig.addCollection("diPreTestScenesWithData", function (collection) {
-    //     let scenes = collection.getFilteredByTag("diPreTestScenes");
-    //     let sceneData = collection.getAll()[0].data.diPreTest.scenes;
-    //     return mergeScenesWithData(scenes, sceneData);
-    // });
-
-    // eleventyConfig.addCollection("hypoDefScenesWithData", function (collection) {
-    //     let scenes = collection.getFilteredByTag("hypoDefScenes");
-    //     let sceneData = collection.getAll()[0].data.hypoDefs.scenes;
-    //     return mergeScenesWithData(scenes, sceneData);
-    // });
-
+    
     return {
         dir: {
             input: "templates",
-            includes: "_includes",
             output: "dist",
         },
         templateFormats: ["njk"]
