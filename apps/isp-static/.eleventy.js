e6c4b12d601be3a953732aaf2bf16678cdcd3343
Scott Silliman <scott.t.silliman@gmail.com>
2020-05-08 17:11:23 -0400

    hopefully have watchTargets and passThruCopies working now

--- apps/isp-static/.eleventy.js
+++ apps/isp-static/.eleventy.js
@@ -1,20 +1,49 @@
 // this config simply copies all of the individually built apps into wwwroot
 
 const APPS = {
-    "../di-instr/dist/": "di-instr",
-    "../di-prepost/dist/*.*": "di-prepost",
-    "../homepage/dist/*.*": ".",
-    "../hypo/dist/*.*": "hypo"
+    "../di-instr/dist": "di-instr",
+    "../di-prepost/dist": "di-prepost",
+    "../homepage/dist": ".",
+    "../hypo/dist": "hypo"
 };
+// {
+//     "di-instr": {
+//         watchFiles: "../di-instr/dist/",
+//         src: "../di-instr/dist/",
+//         dest: "di-instr"
+//     },
+//     // "di-prepost": {
+//     //     watchFiles: "../di-prepost/dist/*.*",
+//     //     src: "../../di-prepost/",
+//     //     dest: "di-prepost"
+//     // },
+//     // "homepage": {
+//     //     watchFiles: "../homepage/dist/*.*",
+//     //     src: "../../homepage/dist/",
+//     //     dest: "."
+//     // },
+//     // "hypo": {
+//     //     watchFiles: "../hypo/dist/*.*",
+//     //     src: "../../hypo/dist/",
+//     //     dest: "hypo"
+//     // }
+// };
+
 
 module.exports = function(eleventyConfig) {
  
     for (let [appSrc, appDest] of Object.entries(APPS)) {
-        eleventyConfig.addWatchTarget(appSrc);
+        eleventyConfig.addWatchTarget(`${appSrc}/*.*`);
         eleventyConfig.addPassthroughCopy({[appSrc]: appDest});
     }
+    // for (let app of Object.entries(APPS)) {
+    //     console.log(process.cwd());
+    //     console.log(app);
+    //     eleventyConfig.addWatchTarget(app.watchFiles);
+    //     eleventyConfig.addPassthroughCopy({[app.src]: app.dest});
+    // }
     
-    eleventyConfig.addPassthroughCopy({"img": "img"});
+    eleventyConfig.addPassthroughCopy("img");
  
     return {
         dir: {
