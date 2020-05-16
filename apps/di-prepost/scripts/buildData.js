f1ff6d7b919c68e7772b666852ed2049802bb51f
Scott Silliman <scott.t.silliman@gmail.com>
2020-05-16 19:24:25 -0400

    added post test data generation

--- apps/di-prepost/scripts/buildData.js
+++ apps/di-prepost/scripts/buildData.js
@@ -49,7 +49,50 @@ class DiPreTestDataBuilder extends DataBuilder {
     }
 }
 
+class DiPostTestDataBuilder extends DataBuilder {
+
+    mungeData(data) {
+        let newData = { scenes: {} };
+        let questions = data.questions;
+        let errorsFound = false;
+        for (let [sceneId, sceneData] of Object.entries(data.scenes)) {
+            sceneData['id'] = sceneId;
+            for (let [transName, transSceneId] of Object.entries(sceneData.transitions)) {
+                if (!VALID_TRANSITION_NAMES.includes(transName)) {
+                    console.error(`ERROR: ${sceneId} has an invalid transition: ${transName}`);
+                    errorsFound = true;
+                }
+                if (!data.scenes.hasOwnProperty(transSceneId)) {
+                    console.error(`ERROR: ${sceneId}.transitions.${transName} points to an invalid scene: ${transSceneId}`);
+                    errorsFound = true;
+                }
+            }
+            sceneData = addSceneTypeIfMissing(sceneData);
+
+            if ("carResults" === sceneData.sceneType) {
+                sceneData.questions = questions.car;
+            } else if ("libraryResults" === sceneData.sceneType) {
+                sceneData.questions = questions.library;
+            }
+            if (sceneData.hasOwnProperty("correctQ1Answer")) {
+                sceneData.questions.q1.correctAnswer = sceneData.correctQ1Answer;
+                delete sceneData.correctA1Answer;
+            }
+
+            sceneData = addCustomEnterActionsIfMissing(sceneData);
+            sceneData = addCustomExitActionsIfMissing(sceneData);
+            newData.scenes[sceneId] = sceneData;
+        }
+        if (errorsFound) {
+            newData = null;
+        }
+        return newData;
+    }
+}
 
 
 let builder = new DiPreTestDataBuilder("diPreTest");
-builder.buildData()
+builder.buildData();
+
+let builder2 = new DiPostTestDataBuilder("diPostTest");
+builder2.buildData();
