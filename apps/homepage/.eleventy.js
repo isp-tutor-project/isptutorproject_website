fe4477e8e5cc0d08e5bb232a0abdbc0eaa696ed7
Scott Silliman <scott.t.silliman@gmail.com>
2020-05-19 09:59:25 -0400

    removed kruft

--- apps/homepage/.eleventy.js
+++ apps/homepage/.eleventy.js
@@ -1,5 +1,3 @@
-// const fs = require("fs");
-// const path = require("path");
 
 const { 
     prettify, 
@@ -7,18 +5,12 @@ const {
     jsonify,
     mergeScenesWithData
 } = require("@isptutorproject/eleventy-config");
-// verticallyCenter,
-
-// const APPS = {
-//     "di-instruction": "di-instruction",
-// };
 
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addTransform("prettier", prettify);
     eleventyConfig.addNunjucksFilter("jsonify", jsonify);
     eleventyConfig.addNunjucksFilter("uuid", uuid);
-    // eleventyConfig.addPairedShortcode("vertcenter", verticallyCenter);
 
     return {
         dir: {
