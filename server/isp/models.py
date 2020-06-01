fc792b4e0b22ddfd4089fd08adf1bbf5b86444ba
HelenaZhouBY <aiying@aiyings-mbp.wifi.cmu.edu>
2020-06-01 14:01:35 -0400

    commit on 'master'

--- server/isp/models.py
+++ server/isp/models.py
@@ -10,6 +10,60 @@ class Module(models.Model):
     def __str__(self):
         return self.name
 
+# an App is named configuration for a Module with 0-more of it's Features enabled
+class Activity(models.Model):
+    name = models.CharField(max_length=64, unique=True, help_text="descriptive name for creating pathways")
+    label = models.CharField(max_length=64, help_text="text students will see on home page")
+    module = models.ForeignKey(Module, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class School(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=64, unique=True, null=True)
+    school = models.ForeignKey(School, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Class(models.Model):
+    classname = models.CharField(max_length=64, unique=True, null=True)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.classname
+
+class Pathway(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+    description = models.TextField(blank=True, null=True)
+
+    constrain_school = models.ForeignKey(School, on_delete=models.CASCADE, null=True)
+    constrain_teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, null=True)
+    constrain_class = models.ForeignKey(Class, on_delete=models.CASCADE, null=True)
+ 
+    def __str__(self):
+         return self.name
+
+class Student(models.Model):
+    pathway = models.ForeignKey(Pathway, on_delete=models.CASCADE)
+    classname = models.ForeignKey(Class, on_delete=models.CASCADE)
+
+class OrderedActivity(models.Model):
+    pathway = models.ForeignKey(Pathway, on_delete=models.CASCADE)
+    order = models.IntegerField(null=False)
+    activity = models.ForeignKey(Activity, on_delete=models.CASCADE, related_name="activities")
+
+    def __str__(self):
+        return "%s" % self.order
+        
+    class Meta:
+        ordering = ('order',)
 
 # a Feature (flag) is simply a string associated with a Module
 class Feature(models.Model):
@@ -23,43 +77,13 @@ class Feature(models.Model):
     class Meta:
         unique_together = [['name', 'module']]
 
-
-# an App is named configuration for a Module with 0-more of it's Features enabled
-class App(models.Model):
-    name = models.CharField(max_length=64, unique=True, help_text="descriptive name for creating pathways")
-    label = models.CharField(max_length=64, help_text="text students will see on home page")
-    module = models.ForeignKey(Module, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.name
-
-
 # i'd like the list of features one can enable to be limited to app.module's features
 class EnabledFeature(models.Model):
-    app = models.ForeignKey(App, on_delete=models.CASCADE)
+    activity = models.ForeignKey(Activity, on_delete=models.CASCADE)
     feature = models.ForeignKey(Feature, on_delete=models.CASCADE, related_name="enabled_features")
 
     def __str__(self):
         return self.feature.name
 
     class Meta:
-        unique_together = [['app', 'feature']]
-
-
-class Pathway(models.Model):
-    name = models.CharField(max_length=64, unique=True)
-    description = models.TextField()
-
-    def __str__(self):
-        return self.name
-        
-class OrderedApp(models.Model):
-    pathway = models.ForeignKey(Pathway, on_delete=models.CASCADE)
-    order = models.IntegerField(null=False)
-    app = models.ForeignKey(App, on_delete=models.CASCADE, related_name="apps")
-
-    def __str__(self):
-        return "%s" % self.order
-        
-    class Meta:
-        ordering = ('order',)
+        unique_together = [['activity', 'feature',]]
