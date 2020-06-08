17eabaf9b08da799dcf677880333a2197ed27444
Scott Silliman <scott.t.silliman@gmail.com>
2020-06-08 07:03:42 -0400

    changed logout implementation. added some methods for changing title/section name

--- common/navbar/index.js
+++ common/navbar/index.js
@@ -1,29 +1,40 @@
 import "./index.css";
 
 export class NavBar {
-    constructor(logoutHandler) {
+    constructor() {
         this.el = document.querySelector("nav.navbar");
         this.userInfoRegion = document.getElementById("user_info_region");
+        this.activityTitle = document.getElementById("activity_title");
+        this.activitySection = document.getElementById("activity_section");
         this.signOutBtn = document.getElementById("sign_out_button");
         this.signInText = document.getElementById("sign_in_text");
         this.signOutUser = this.signOutUser.bind(this);
-        this.logoutHandler = logoutHandler;
+        // this.logoutHandler = logoutHandler;
         this.signOutBtn.addEventListener("click", this.signOutUser);
     }
 
     signOutUser(e) {
         this.userInfoRegion.classList.add("invisible");
-        if (this.logoutHandler) {
-            this.logoutHandler();
-        } else {
-            window.location.path = "/";
-        }
+        // if (this.logoutHandler) {
+        //     this.logoutHandler();
+        // } else {
+        localStorage.clear();
+        window.location.path = "/";
+        // }
         // localStorage.removeItem("CLASSCODE");
         // localStorage.remove("USERID");
     }
 
+    displayActivityTitle(title) {
+        this.activityTitle.innerText = title;
+    }
+
+    displayActivitySection(section) {
+        this.activitySection.innerText = section;
+    }
+
     displayUser(userName) {
-        this.signInText.innerHTML = `Welcome ${userName}`;
+        this.signInText.innerHTML = `Welcome, ${userName}`;
         this.userInfoRegion.classList.remove("invisible");
     }
 };
