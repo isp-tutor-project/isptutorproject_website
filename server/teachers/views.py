82f55ac86f14a032ba1487a66bf09d1613cb435e
HelenaZhouBY <aiying@aiyings-mbp.wifi.cmu.edu>
2020-06-08 12:59:38 -0400

    added teacheradmin

--- server/teachers/views.py
+++ server/teachers/views.py
@@ -2,24 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-# Create class page
-def createclass(request):
-    return render(request, 'createclass.html')
-
 # Add students
-def addstudents(request):
+def addstudents(request, classcode):
     return render(request, 'addstudents.html')
 
 # Assign students
 def assignstudents(request):
     return render(request, 'assignstudents.html')
 
-# mock index page
-def index(request):
-    if (request.GET.get('btn_1')):
-        return createclass(request)
-    elif (request.GET.get('btn_2')):
-        return addstudents(request)
-    elif (request.GET.get('btn_3')):
-        return assignstudents(request)
-    return render(request, 'index.html')
