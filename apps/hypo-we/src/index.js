79b84b9e0686ca80ffe060a2e2e1bfe14fcbcbe1
Scott Silliman <scott.t.silliman@gmail.com>
2020-06-10 16:15:19 -0400

    default implementation

--- apps/hypo-we/src/index.js
+++ apps/hypo-we/src/index.js
@@ -1,2 +1,8 @@
 import "./styles/index.scss";
 
+import { HypoWEApp } from "./hypo-we-app";
+
+let appData = require("../data/hypoWE.json");
+
+let app = new HypoWEApp(appData);
+app.setStartScene("selectVIN");
