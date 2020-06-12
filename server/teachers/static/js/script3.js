0705d18f5157fbb64ec81e4ebea779be1d779bb8
HelenaZhouBY <aiying@aiyings-mbp.wifi.cmu.edu>
2020-06-11 22:40:29 -0400

    modified edit page

--- server/teachers/static/js/script3.js
+++ server/teachers/static/js/script3.js
@@ -1,6 +1,7 @@
 $(document).ready(function()
 {   
-
+    $("#class").prop("readonly",true);
+    $("#classcode").prop("readonly",true);
 	//ajax row data
 	var ajax_data = []
 	var random_id = function() 
@@ -11,16 +12,17 @@ $(document).ready(function()
 		return id_num + id_str;
 	};
 	//--->create data table > start
-	var tbl = '';
-	tbl +='<table class="table table-hover" id="tbl_posts">'
+    var tbl = '';
+    tbl +='<div class="table-wrapper">';
+	tbl +='<table class="table table-hover" id="tbl_posts">';
 
 		//--->create table header > start
 		tbl +='<thead>';
 			tbl +='<tr>';
-			tbl +='<th>Select</th>';
+			tbl +='<th></th>';
 			tbl +='<th>User ID</th>';
       		tbl +='<th>Pathway</th>';
-      		tbl +='<th>Options</th>';
+      		tbl +='<th></th>';
 			tbl +='</tr>';
 		tbl +='</thead>';
 		//--->create table header > end
@@ -40,19 +42,7 @@ $(document).ready(function()
 					tbl +='<td ><div class="row_data" col_name="userid">'+val['userid']+'</div></td>';
           			tbl +='<td ><div class="row_data" col_name="pathway">'+val['pathway']+'</div></td>';
           			tbl +='<td ><div class="row_data" col_name="selections">'+val['selections']+'</div></td>';
-
-					//--->edit options > start
-					tbl +='<td>';
-					
-						tbl +='<span class="btn_edit" > <a href="#" class="btn btn-link " row_id="'+row_id+'" > Edit </a> </span>';
-						//only show this button if edit button is clicked
-						tbl +='<span class="btn_save"> <a href="#" class="btn btn-link"  row_id="'+row_id+'"> Save</a> | </span>';
-						tbl +='<span class="btn_cancel"> <a href="#" class="btn btn-link" row_id="'+row_id+'"> Cancel</a> | </span>';
-                        tbl +='<span class="btn_delete"> <a href="#" class="btn btn-link" row_id="'+row_id+'" > Delete Row</a> </span>';
-
-					tbl +='</td>';
-					//--->edit options > end
-					
+					tbl +='<td ><div class="btn_delete"><a href="#" class="btn btn-link" row_id="'+row_id+'">Delete Row</a></div></td>';
 				tbl +='</tr>';
 			});
             //--->create table body rows > end
@@ -60,178 +50,12 @@ $(document).ready(function()
 		tbl +='</tbody>';
 		//--->create table body > end
 
-	tbl +='</table>'	
+	tbl +='</table>';
 	//--->create data table > end
-
+    tbl +='</div>';
 	//out put table data
 	$(document).find('.tbl_user_data').html(tbl);
-	$(document).find('.btn_save').hide();
-	$(document).find('.btn_cancel').hide();
-    $(document).find('.btn_delete').hide();
-
-
-	//--->make div editable > start
-	$(document).on('click', '.row_data', function(event) 
-	{
-		event.preventDefault(); 
-
-		if($(this).attr('edit_type') == 'button')
-		{
-			return false; 
-		}
-
-		//make div editable
-		$(this).closest('div').attr('contenteditable', 'true');
-		//add bg css
-		$(this).addClass('bg-warning').css('padding','5px');
-
-		$(this).focus();
-	})	
-	//--->make div editable > end
-
-
-	//--->save single field data > start
-	$(document).on('focusout', '.row_data', function(event) 
-	{
-		event.preventDefault();
-
-		if($(this).attr('edit_type') == 'button')
-		{
-			return false; 
-		}
-
-		var row_id = $(this).closest('tr').attr('row_id'); 
-		
-		var row_div = $(this)				
-		.removeClass('bg-warning') //add bg css
-		.css('padding','')
-
-		var col_name = row_div.attr('col_name'); 
-		var col_val = row_div.html(); 
-
-		var arr = {};
-		arr[col_name] = col_val;
-
-		//use the "arr"	object for your ajax call
-		$.extend(arr, {row_id:row_id});
-
-		//out put to show
-		$('.post_msg').html( '<pre class="bg-success">'+JSON.stringify(arr, null, 2) +'</pre>');
-		
-	})	
-	//--->save single field data > end
-
- 
-	//--->button > edit > start	
-	$(document).on('click', '.btn_edit', function(event) 
-	{
-		event.preventDefault();
-		var tbl_row = $(this).closest('tr');
-
-		var row_id = tbl_row.attr('row_id');
-
-		tbl_row.find('.btn_save').show();
-		tbl_row.find('.btn_cancel').show();
-    	tbl_row.find('.btn_delete').show();
-
-		//hide edit button
-		tbl_row.find('.btn_edit').hide(); 
-
-		//make the whole row editable
-		tbl_row.find('.row_data')
-		.attr('contenteditable', 'true')
-		.attr('edit_type', 'button')
-		.addClass('bg-warning')
-		.css('padding','3px')
-
-		//--->add the original entry > start
-		tbl_row.find('.row_data').each(function(index, val) 
-		{  
-			//this will help in case user decided to click on cancel button
-			$(this).attr('original_entry', $(this).html());
-		}); 		
-		//--->add the original entry > end
-
-	});
-	//--->button > edit > end
-
-
-	//--->button > cancel > start	
-	$(document).on('click', '.btn_cancel', function(event) 
-	{
-		event.preventDefault();
-
-		var tbl_row = $(this).closest('tr');
-
-		var row_id = tbl_row.attr('row_id');
-
-		//hide save and cancel buttons
-		tbl_row.find('.btn_save').hide();
-		tbl_row.find('.btn_cancel').hide();
-    	tbl_row.find('.btn_delete').hide();
-		//show edit button
-		tbl_row.find('.btn_edit').show();
-
-		//make the whole row editable
-		tbl_row.find('.row_data')
-		.attr('edit_type', 'click')
-		.removeClass('bg-warning')
-		.css('padding','') 
-
-		tbl_row.find('.row_data').each(function(index, val) 
-		{   
-			$(this).html( $(this).attr('original_entry') ); 
-		});  
-	});
-	//--->button > cancel > end
-
-	
-	//--->save whole row entery > start	
-	$(document).on('click', '.btn_save', function(event) 
-	{
-		event.preventDefault();
-		var tbl_row = $(this).closest('tr');
-
-		var row_id = tbl_row.attr('row_id');
-
-		
-		//hide save and cacel buttons
-		tbl_row.find('.btn_save').hide();
-		tbl_row.find('.btn_cancel').hide();
-    	tbl_row.find('.btn_delete').hide();
-
-		//show edit button
-		tbl_row.find('.btn_edit').show();
-
-
-		//make the whole row editable
-		tbl_row.find('.row_data')
-		.attr('edit_type', 'click')
-		.removeClass('bg-warning')
-		.css('padding','') 
-
-		//--->get row data > start
-		var arr = {}; 
-		tbl_row.find('.row_data').each(function(index, val) 
-		{   
-			var col_name = $(this).attr('col_name');  
-			var col_val  =  $(this).html();
-			arr[col_name] = col_val;
-		});
-		//--->get row data > end
-
-		//use the "arr"	object for your ajax call
-		$.extend(arr, {row_id:row_id});
-
-		//out put to show
-		$('.post_msg').html( '<pre class="bg-success">'+JSON.stringify(arr, null, 2) +'</pre>')
-		 
-
-	});
-    //--->save whole row entery > end
-
-
-}); 
+});
 
 jQuery(document).delegate('.btn_delete', 'click', function(e) {
      e.preventDefault();
@@ -240,7 +64,6 @@ jQuery(document).delegate('.btn_delete', 'click', function(e) {
       var id = jQuery(this).attr('row_id');
       var row_div = jQuery(this).attr('row_div');
       jQuery('#rec-' + id).remove();
-      
     $('#tbl_posts_body tr').each(function(index) {
       $(this).find('span.sn').html(index+1);
     });
@@ -306,4 +129,17 @@ function exportTableToExcel(table_ID, filename=''){
     }
     
     saveAs(new Blob([s2ab(wbout)],{type:"application/octet-stream"}), filename);
+}
+
+function edit1(){
+    $("#class").prop("readonly",false);
+}
+function save1(){
+    $("#class").prop("readonly",true);
+}
+function edit2(){
+    $("#classcode").prop("readonly",false);
+}
+function save2(){
+    $("#classcode").prop("readonly",true);
 }
