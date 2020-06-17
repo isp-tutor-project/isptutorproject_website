ac06b6e752f7e497b86c775b247c31a4738e161e
Scott Silliman <scott.t.silliman@gmail.com>
2020-06-17 15:50:38 -0400

    added a new field 'module' to App which is an enum field. changed the length of some charfields

--- server/isp/models.py
+++ server/isp/models.py
@@ -1,10 +1,31 @@
 from django.db import models
 
+PRE = '00PRE'
+RQ = '05RQ'
+BRM = '10BRM'
+HYPO = '15HYPO'
+DI = '20DI'
+POST = '25POST'
+
+MODULE_CHOICES = [
+    (PRE, 'Pre-Training'),
+    (RQ,  'Research Question'),
+    (BRM, 'Basic Research'),
+    (HYPO, 'Hypothesis'),
+    (DI, 'Data-Interpretation'),
+    (POST, 'Post-Training')
+]
+
+MODULE_LABELS = { key:value  for (key, value) in MODULE_CHOICES }
+
 # a App is simply a named URL with 0-more config options (feature flags)
 class App(models.Model):
+    # prepending with numbers for sorting purposes
+
     name = models.CharField(max_length=64, unique=True)
-    description = models.TextField(blank=True)
-    url = models.CharField(max_length=128, help_text="can be path only")
+    description = models.CharField(max_length=256, default="", blank=True)
+    url = models.CharField(max_length=256, help_text="can be path only")
+    module = models.CharField(max_length=8, choices=MODULE_CHOICES, default=PRE)
 
     def __str__(self):
         return self.name
@@ -28,15 +49,17 @@ class Feature(models.Model):
 # an Activity is named configuration for an App with 0-more of it's Features enabled
 class Activity(models.Model):
     name = models.CharField(max_length=64, unique=True, help_text="descriptive name for creating pathways")
+    description = models.CharField(max_length=256, default="", blank=True)
     label = models.CharField(max_length=64, help_text="text students will see on home page")
     app = models.ForeignKey(App, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.name
+        return "%s : %s" % (MODULE_LABELS[self.app.module], self.name)
 
     class Meta:
         verbose_name_plural = "Activities"
-    
+        ordering = ['app__module']
+
 # i'd like the list of features one can enable to be limited to app.module's features
 class EnabledFeature(models.Model):
     activity = models.ForeignKey(Activity, on_delete=models.CASCADE)
@@ -57,7 +80,7 @@ class School(models.Model):
 
 
 class Teacher(models.Model):
-    name = models.CharField(max_length=64, unique=True, null=True)
+    name = models.CharField(max_length=128, blank=False)
     school = models.ForeignKey(School, on_delete=models.CASCADE)
 
     def __str__(self):
