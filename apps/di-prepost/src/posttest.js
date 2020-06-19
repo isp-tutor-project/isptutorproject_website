af2bccb69a8a32680e065880e86361fdee80eb42
Scott Silliman <scott.t.silliman@gmail.com>
2020-06-19 12:16:32 -0400

    copied over some probably unused code from pre-test

--- apps/di-prepost/src/posttest.js
+++ apps/di-prepost/src/posttest.js
@@ -2,6 +2,22 @@ import { DiTestApp, DiTestScene, DiTestResultsScene } from "./index";
 
 const postTestData = require("../data/diPostTest.json");
 
+function handleQ1Submit(e) {
+    console.log("clicked");
+    e.preventDefault();
+    let btn = e.target;
+    let sceneId = btn.id.split('_')[0];
+    let form = document.getElementById(`${sceneId}_q1_form`);
+    if (form.checkValidity()) {
+        let selected = form.querySelector('input[type="radio"]:checked');
+        console.log(selected.value, selected.labels[0].innerText);
+    } else {
+        console.log("invalid");
+        let errMsg = document.getElementById(`${sceneId}_notification_region`);
+        console.log(errMsg);
+        errMsg.classList.remove("invisible");
+    }
+}
 
 class CarResultsScene extends DiTestResultsScene {
     constructor(app, sceneInfo) {
