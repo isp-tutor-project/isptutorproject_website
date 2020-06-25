53e0478e5ba108a637adcf54246b52184b38b13f
Scott Silliman <scott.t.silliman@gmail.com>
2020-06-25 09:55:43 -0400

    rm School model. Teacher.school is now charfield.  added Module and ActivityType models

--- server/isp/admin.py
+++ server/isp/admin.py
@@ -1,15 +1,20 @@
 from django.contrib import admin
 
 from .models import (
-    App, Feature, Activity, EnabledFeature, School, Teacher,
-    Student, Class, Pathway, OrderedActivity
+    Module, App, Feature, ActivityType, Activity, EnabledFeature,
+    Pathway, OrderedActivity
 )
+# Teacher, Class, Student
+
 
 class FeatureInline(admin.TabularInline):
     model = Feature
+    extra = 0
+
 
 class EnabledFeatureInline(admin.TabularInline):
     model = EnabledFeature
+    extra = 0
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         field = super().formfield_for_foreignkey(db_field, request, **kwargs)
@@ -18,59 +23,79 @@ class EnabledFeatureInline(admin.TabularInline):
             field.queryset = field.queryset.filter(app__exact=activity.app.id)
         return field
 
-class TeacherInline(admin.TabularInline):
-    model = Teacher
-    show_change_link = True
-
-
-class PathwayInline(admin.TabularInline):
-    model = Pathway
-    show_change_link = True
-    extra = 0
-
-class ClassInline(admin.TabularInline):
-    model = Class
-    show_change_link = True
-    extra = 0
-
-class StudentInline(admin.TabularInline):
-    model = Student
 
 class OrderedActivityInline(admin.TabularInline):
     model = OrderedActivity
+    extra = 0
+
+
+# class PathwayInline(admin.TabularInline):
+#     model = Pathway
+#     show_change_link = True
+#     extra = 0
+
+
+# class TeacherInline(admin.TabularInline):
+#     model = Teacher
+#     show_change_link = True
+
+
+# class ClassInline(admin.TabularInline):
+#     model = Class
+#     show_change_link = True
+#     extra = 0
+
+
+# class StudentInline(admin.TabularInline):
+#     model = Student
+
+@admin.register(Module)
+class ModuleAdmin(admin.ModelAdmin):
+    list_display = ('order', 'name', 'description')
+
 
 @admin.register(App)
 class AppAdmin(admin.ModelAdmin):
-    list_display = ('name',)
+    list_display = ('name', 'module', 'description')
     inlines = [ FeatureInline ]
+
+
+@admin.register(ActivityType)
+class ActivityTypeAdmin(admin.ModelAdmin):
+    list_display = ('order', 'name', 'description')
+
+
+def app_module(obj):
+    return obj.app.module
+
 
 @admin.register(Activity)
 class ActivityAdmin(admin.ModelAdmin):
-    list_display = ('name', 'app')
+    list_display = ('name', 'app', app_module, 'activity_type', 'description')
     inlines = [ EnabledFeatureInline ]
 
     def get_inline_instances(self, request, obj=None):
         request._obj = obj
         return [
-            inline(self.model, self.admin_site) 
+            inline(self.model, self.admin_site)
             for inline in self.inlines
             if obj is not None
         ]
 
-@admin.register(School)
-class SchoolAdmin(admin.ModelAdmin):
-    inlines = [ TeacherInline ]
 
-@admin.register(Teacher)
-class TeacherAdmin(admin.ModelAdmin):
-    inlines = [ PathwayInline, ClassInline ]
-
-@admin.register(Class)
-class ClassAdmin(admin.ModelAdmin):
-    inlines = [ StudentInline ]
-    
 @admin.register(Pathway)
 class PathwayAdmin(admin.ModelAdmin):
     list_display = ('name', 'description')
     inlines = [ OrderedActivityInline ]
 
+
+
+
+# @admin.register(Teacher)
+# class TeacherAdmin(admin.ModelAdmin):
+#     inlines = [ PathwayInline, ClassInline ]
+
+
+# @admin.register(Class)
+# class ClassAdmin(admin.ModelAdmin):
+#     inlines = [ StudentInline ]
--- server/isp/models.py
+++ server/isp/models.py
@@ -1,43 +1,50 @@
 from django.db import models
 
-PRE = '00PRE'
-RQ = '05RQ'
-BRM = '10BRM'
-HYPO = '15HYPO'
-DI = '20DI'
-POST = '25POST'
 
-MODULE_CHOICES = [
-    (PRE, 'Pre-Training'),
-    (RQ,  'Research Question'),
-    (BRM, 'Basic Research'),
-    (HYPO, 'Hypothesis'),
-    (DI, 'Data-Interpretation'),
-    (POST, 'Post-Training')
-]
+class Module(models.Model):
+    order = models.IntegerField(blank=False, unique=True)
+    name = models.CharField(max_length=64, blank=False)
+    description = models.CharField(max_length=256, blank=True, default="")
 
-MODULE_LABELS = { key:value  for (key, value) in MODULE_CHOICES }
+    def __str__(self):
+        return self.name
 
-# a App is simply a named URL with 0-more config options (feature flags)
+    class Meta:
+        ordering = ['order']
+
+
 class App(models.Model):
-    # prepending with numbers for sorting purposes
-
-    name = models.CharField(max_length=64, unique=True)
-    description = models.CharField(max_length=256, default="", blank=True)
-    url = models.CharField(max_length=256, help_text="can be path only")
-    module = models.CharField(max_length=8, choices=MODULE_CHOICES, default=PRE)
+    """
+    An App is simply a named URL with 0-more config options(feature flags)
+    """
+    name = models.CharField(max_length=64,
+                            unique=True)
+    description = models.CharField(max_length=256,
+                                  default="",
+                                  blank=True)
+    url = models.CharField(max_length=256,
+                           help_text="can be path only")
+    module = models.ForeignKey(Module,
+                               null=False,
+                               on_delete=models.PROTECT)
 
     def __str__(self):
         return self.name
 
 
-# a Feature (flag) is simply a string associated with a App which modifies it's
-# behavior
+    class Meta:
+        ordering = ['module__order', 'name']
+
+
 class Feature(models.Model):
+    """
+    A Feature is simply a string constant which modifies an App's behavior
+    """
     name = models.CharField(max_length=32)
     description = models.CharField(max_length=128)
-    app = models.ForeignKey(App, on_delete=models.CASCADE, related_name="available_features")
-
+    app = models.ForeignKey(App,
+                            on_delete=models.CASCADE,
+                            related_name="available_features")
 
     def __str__(self):
         return "%s (%s) " % (self.name, self.description)
@@ -46,24 +53,54 @@ class Feature(models.Model):
         unique_together = [['name', 'app']]
 
 
-# an Activity is named configuration for an App with 0-more of it's Features enabled
-class Activity(models.Model):
-    name = models.CharField(max_length=64, unique=True, help_text="descriptive name for creating pathways")
-    description = models.CharField(max_length=256, default="", blank=True)
-    label = models.CharField(max_length=64, help_text="text students will see on home page")
-    app = models.ForeignKey(App, on_delete=models.CASCADE)
+
+class ActivityType(models.Model):
+    order = models.IntegerField(blank=False, unique=True)
+    name = models.CharField(max_length=64, blank=False)
+    description = models.CharField(max_length=256, blank=True, default="")
 
     def __str__(self):
-        return "%s : %s" % (MODULE_LABELS[self.app.module], self.name)
+        return self.name
+
+    class Meta:
+        ordering = ['order']
+
+
+class Activity(models.Model):
+    """
+    An Activity is named config for an App with 0-more of it's Features enabled
+    """
+    name = models.CharField(max_length=64,
+                            unique=True,
+                            help_text="descriptive name for creating pathways")
+    description = models.CharField(max_length=256,
+                                   default="",
+                                   blank=True)
+    label = models.CharField(max_length=64,
+                             help_text="text students will see on home page")
+    app = models.ForeignKey(App,
+                            on_delete=models.CASCADE)
+    activity_type = models.ForeignKey(ActivityType,
+                                      null=False,
+                                      on_delete=models.PROTECT)
+
+    def __str__(self):
+        return "%s : %s" % (self.app.module.name, self.name)
 
     class Meta:
         verbose_name_plural = "Activities"
-        ordering = ['app__module']
+        ordering = ['app__module__order', 'activity_type__order', 'name']
 
-# i'd like the list of features one can enable to be limited to app.module's features
+
 class EnabledFeature(models.Model):
-    activity = models.ForeignKey(Activity, on_delete=models.CASCADE)
-    feature = models.ForeignKey(Feature, on_delete=models.CASCADE, related_name="enabled_features")
+    """
+    An App's feature (constrained to .app.features), enabled for an Activity
+    """
+    activity = models.ForeignKey(Activity,
+                                 on_delete=models.CASCADE)
+    feature = models.ForeignKey(Feature,
+                                on_delete=models.CASCADE,
+                                related_name="enabled_features")
 
     def __str__(self):
         return self.feature.name
@@ -72,16 +109,18 @@ class EnabledFeature(models.Model):
         unique_together = [['activity', 'feature',]]
 
 
-class School(models.Model):
-    name = models.CharField(max_length=64, unique=True)
+# class School(models.Model):
+#     name = models.CharField(max_length=64, unique=True)
 
-    def __str__(self):
-        return self.name
+#     def __str__(self):
+#         return self.name
 
 
 class Teacher(models.Model):
-    name = models.CharField(max_length=128, blank=False)
-    school = models.ForeignKey(School, on_delete=models.CASCADE)
+    name = models.CharField(max_length=128,
+                            blank=False)
+    school = models.CharField(max_length=128,
+                              default="")
 
     def __str__(self):
         return self.name
@@ -91,9 +130,13 @@ class Teacher(models.Model):
 
 
 class Pathway(models.Model):
-    name = models.CharField(max_length=64, unique=False)
-    description = models.CharField(max_length=128, default="", blank=True)
-    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    name = models.CharField(max_length=64,
+                            unique=False)
+    description = models.CharField(max_length=128,
+                                   default="",
+                                   blank=True)
+    teacher = models.ForeignKey(Teacher,
+                                on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
@@ -101,10 +144,28 @@ class Pathway(models.Model):
     class Meta:
         unique_together = [['name', 'teacher']]
 
+
+class OrderedActivity(models.Model):
+    pathway = models.ForeignKey(Pathway,
+                                on_delete=models.CASCADE)
+    order = models.IntegerField(null=False)
+    activity = models.ForeignKey(Activity,
+                                 on_delete=models.CASCADE,
+                                 related_name="activities")
+
+    def __str__(self):
+        return "%d" % self.order
+
+    class Meta:
+        ordering = ('order',)
+
+
 class Class(models.Model):
-    name = models.CharField(max_length=64, null=False, help_text="teacher friendly name")
-    classcode = models.CharField(max_length=64, unique=True, null=False, help_text="unique name for class")
-    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    name = models.CharField(max_length=64,
+                            null=False,
+                            help_text="teacher friendly name")
+    teacher = models.ForeignKey(Teacher,
+                                on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
@@ -115,20 +176,15 @@ class Class(models.Model):
 
 
 class Student(models.Model):
-    name = models.CharField(max_length=128, null=False)
-    pathway = models.ForeignKey(Pathway, null=True, blank=True, on_delete=models.CASCADE)
-    classname = models.ForeignKey(Class, on_delete=models.CASCADE)
+    name = models.CharField(max_length=128,
+                            null=False)
+    pathway = models.ForeignKey(Pathway,
+                                null=True,
+                                blank=True,
+                                on_delete=models.CASCADE)
+    classname = models.ForeignKey(Class,
+                                  on_delete=models.CASCADE)
 
     def __str__(self):
          return self.name
 
-class OrderedActivity(models.Model):
-    pathway = models.ForeignKey(Pathway, on_delete=models.CASCADE)
-    order = models.IntegerField(null=False)
-    activity = models.ForeignKey(Activity, on_delete=models.CASCADE, related_name="activities")
-
-    def __str__(self):
-        return "%d" % self.order
-
-    class Meta:
-        ordering = ('order',)
