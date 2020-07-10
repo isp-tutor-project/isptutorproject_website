cbaba0e3d7828d9fc7211580d88a0d0ef5174e19
Scott Silliman <scott.t.silliman@gmail.com>
2020-07-10 09:07:57 -0400

    navbar and db initialization

--- apps/hypo-we/src/index.js
+++ apps/hypo-we/src/index.js
@@ -1,6 +1,16 @@
 import "./styles/index.scss";
+// import "@isptutorproject/navbar/index.css";
+
+import { NavBar } from "@isptutorproject/navbar";
+import { getDBConnection } from "@isptutorproject/isp-database";
 
 function initApp() {
+    const navbar = new NavBar();
+    const userID = localStorage.getItem("userID");
+
+    console.log(`userName: ${userID}`);
+    navbar.displayActivityTitle("Hypothesis Lesson");
+    navbar.displayUser(userID);
     console.log("app initted");
 }
 
