86ced1e46bfe514a59d374c5d1e468da0194b991
Scott Silliman <scott.t.silliman@gmail.com>
2020-07-16 22:50:20 -0400

    no longer shows snackbar if msg is undefined/null

--- common/snackbar/index.js
+++ common/snackbar/index.js
@@ -7,14 +7,16 @@ export class SnackBar {
     }
 
     show(text) {
-        this.el.innerHTML = text;
-        // Add the "show" class to DIV
-        // this.el.className = "show";
-        this.el.classList.add("show");
-        // After 3 seconds, remove the show class from DIV
-        setTimeout(() => {
-            // snackbar.className = snackbar.className.replace("show", "");
-            this.el.classList.remove("show");
-        }, 3000);    
+        if (typeof(text) !== "undefined" && null !== text) {
+            this.el.innerHTML = text;
+            // Add the "show" class to DIV
+            // this.el.className = "show";
+            this.el.classList.add("show");
+            // After 3 seconds, remove the show class from DIV
+            setTimeout(() => {
+                // snackbar.className = snackbar.className.replace("show", "");
+                this.el.classList.remove("show");
+            }, 3000);
+        }
     }
 }
