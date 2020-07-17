3e8e4b78f579a4c3fb520427b9d9e0b4af7fb29a
Scott Silliman <scott.t.silliman@gmail.com>
2020-07-16 23:27:04 -0400

    added minimum config to use localstorage db

--- apps/di-instr/src/index.js
+++ apps/di-instr/src/index.js
@@ -1,11 +1,21 @@
 import "./styles/index.scss";
 
 import { SnackBar } from "@isptutorproject/snackbar";
+import { getDBConnection } from "@isptutorproject/isp-database";
 import { DiInstructionApp } from "./di-instruction-app";
 
 
 let appData = require("../data/diInstr.json");
 
+let db = getDBConnection("localstorage");
+let userID = localStorage.getItem("userID");
+let classCode = localStorage.getItem("classCode");
+let currentActivity = localStorage.getItem("currentActivity");
+let features = localStorage.getItem("currentActivityFeatures")
+               .split(":")
+               .filter((i) => i !== ":");
+db.setCredentials(classCode, userID);
+let app = new DiInstructionApp(appData, db, currentActivity, features);
 let snackbar = new SnackBar();
 let app = new DiInstructionApp(appData)
 app.setSnackbar(snackbar);
