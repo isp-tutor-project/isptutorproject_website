fb469fbb3c47fe39c7bfa5cd6b83f755f6b4358a
Scott Silliman <scott.t.silliman@gmail.com>
2020-07-19 17:07:47 -0400

    now makes use of getActivityConfiguration() and passing the result to the app rather than setting up db connection manually

--- apps/di-instr/src/index.js
+++ apps/di-instr/src/index.js
@@ -1,21 +1,15 @@
 import "./styles/index.scss";
 
 import { SnackBar } from "@isptutorproject/snackbar";
-import { getDBConnection } from "@isptutorproject/isp-database";
+import { getActivityConfiguration } from "@isptutorproject/activity-config";
+// import { getDBConnection } from "@isptutorproject/isp-database";
 import { DiInstructionApp } from "./di-instruction-app";
 
+let activityConfig = getActivityConfiguration();
 
 let appData = require("../data/diInstr.json");
 
-let db = getDBConnection("localstorage");
-let userID = localStorage.getItem("userID");
-let classCode = localStorage.getItem("classCode");
-let currentActivity = localStorage.getItem("currentActivity");
-let features = localStorage.getItem("currentActivityFeatures")
-               .split(":")
-               .filter((i) => i !== ":");
-db.setCredentials(classCode, userID);
-let app = new DiInstructionApp(appData, db, currentActivity, features);
+let app = new DiInstructionApp(appData, activityConfig); //db, currentActivity, features);
 let snackbar = new SnackBar();
 app.setSnackbar(snackbar);
 if (process.env.NODE_ENV !== "production") {
