4dfcd8cd78ee82e5d7cfa5881e7cf79ade7e73ff
Scott Silliman <scott.t.silliman@gmail.com>
2020-07-20 16:20:36 -0400

    no more need for separate DiPre/PostTestApp subclasses.  now simply pass different params to DiTestApp constructor

--- apps/di-prepost/src/posttest.js
+++ apps/di-prepost/src/posttest.js
@@ -1,54 +1,22 @@
-import { DiTestApp, DiTestScene, DiTestResultsScene } from "./index";
+import { DiTestApp } from "./index";
 
-const postTestData = require("../data/diPostTest.json");
+import { getActivityConfiguration } from "@isptutorproject/activity-config";
 
-function handleQ1Submit(e) {
-    console.log("clicked");
-    e.preventDefault();
-    let btn = e.target;
-    let sceneId = btn.id.split('_')[0];
-    let form = document.getElementById(`${sceneId}_q1_form`);
-    if (form.checkValidity()) {
-        let selected = form.querySelector('input[type="radio"]:checked');
-        console.log(selected.value, selected.labels[0].innerText);
-    } else {
-        console.log("invalid");
-        let errMsg = document.getElementById(`${sceneId}_notification_region`);
-        console.log(errMsg);
-        errMsg.classList.remove("invisible");
-    }
+const DEFAULT_APP_INITIAL_STATE = {
+    events: [],
+    // mapping of sceneIDs -> scene-type specific data
+    sceneState: {},
+    currentScene: "start"
+};
+
+
+let activityConfig = getActivityConfiguration();
+const activityData = require("../data/diPostTest.json");
+
+let app = new DiTestApp(
+    activityData, activityConfig, DEFAULT_APP_INITIAL_STATE
+);
+if (process.env.NODE_ENV === "development") {
+    app.setStartScene("start");
 }
-
-class CarResultsScene extends DiTestResultsScene {
-    constructor(app, sceneInfo) {
-        super(app, sceneInfo);
-    }
-}
-
-class LibraryResultsScene extends DiTestResultsScene {
-    constructor(app, sceneInfo) {
-        super(app, sceneInfo);
-    }
-}
-
-class DiPostTestApp extends DiTestApp {
-
-    createScene(sceneInfo) {
-        let scene;
-        console.log("DiPostTestApp::createScene()", sceneInfo);
-        switch (sceneInfo.sceneType) {
-            case "carResults":
-                scene = new CarResultsScene(this, sceneInfo);
-                break;
-            case "libraryResults":
-                scene = new LibraryResultsScene(this, sceneInfo);
-                break;
-            default:
-                scene = new DiTestScene(this, sceneInfo);
-        }
-        return scene;
-    }
-}
-
-let app = new DiPostTestApp(postTestData, "diposttest");
-app.setStartScene("start");
+app.start();
--- apps/di-prepost/src/pretest.js
+++ apps/di-prepost/src/pretest.js
@@ -1,55 +1,26 @@
+import { DiTestApp } from "./index";
 
-import { DiTestApp, DiTestScene, DiTestResultsScene } from "./index";
+import { getActivityConfiguration } from "@isptutorproject/activity-config";
 
-const preTestData = require("../data/diPreTest.json");
 
-function handleQ1Submit(e) {
-    console.log("clicked");
-    e.preventDefault();
-    let btn = e.target;
-    let sceneId = btn.id.split('_')[0];
-    let form = document.getElementById(`${sceneId}_q1_form`);
-    if (form.checkValidity()) {     
-        let selected = form.querySelector('input[type="radio"]:checked');
-        console.log(selected.value, selected.labels[0].innerText);
-    } else {
-        console.log("invalid");
-        let errMsg = document.getElementById(`${sceneId}_notification_region`);
-        console.log(errMsg);
-        errMsg.classList.remove("invisible");
-    }
+const DEFAULT_APP_INITIAL_STATE = {
+    events: [],
+    // mapping of sceneIDs -> scene-type specific data
+    sceneState: {},
+    currentScene: "start"
+};
+
+
+let activityConfig = getActivityConfiguration();
+const activityData = require("../data/diPreTest.json");
+
+// console.log(activityConfig);
+
+let app = new DiTestApp(
+    activityData, activityConfig, DEFAULT_APP_INITIAL_STATE
+);
+if (process.env.NODE_ENV === "development") {
+    app.setStartScene("start");
 }
+app.start()
 
-class MemoryResultsScene extends DiTestResultsScene {
-    constructor(app, sceneInfo) {
-        super(app, sceneInfo);
-    }
-}
-
-class RocketsResultsScene extends DiTestResultsScene {
-    constructor(app, sceneInfo) {
-        super(app, sceneInfo);
-    }
-}
-
-class DiPreTestApp extends DiTestApp {
-
-    createScene(sceneInfo) {
-        let scene;
-        console.log("DiPreTestApp::createScene()", sceneInfo);
-        switch(sceneInfo.sceneType) {
-            case "memoryResults":
-                scene = new MemoryResultsScene(this, sceneInfo);
-                break;
-            case "rocketsResults":
-                scene = new RocketsResultsScene(this, sceneInfo);
-                break;
-            default:
-                scene = new DiTestScene(this, sceneInfo);
-        }
-        return scene;
-    }
-}
-
-let app = new DiPreTestApp(preTestData, "dipretest");
-app.setStartScene("start");
