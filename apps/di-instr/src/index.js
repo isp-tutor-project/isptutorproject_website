d1b9302ee5d1fab6a55e16f053947a4b79b8fd45
Scott Silliman <scott.t.silliman@gmail.com>
2020-07-19 20:14:48 -0400

    now very minimal. snackbar is now created by base class

--- apps/di-instr/src/index.js
+++ apps/di-instr/src/index.js
@@ -1,18 +1,13 @@
 import "./styles/index.scss";
 
-import { SnackBar } from "@isptutorproject/snackbar";
 import { getActivityConfiguration } from "@isptutorproject/activity-config";
-// import { getDBConnection } from "@isptutorproject/isp-database";
 import { DiInstructionApp } from "./di-instruction-app";
 
 let activityConfig = getActivityConfiguration();
+let activityData = require("../data/diInstr.json");
 
-let appData = require("../data/diInstr.json");
-
-let app = new DiInstructionApp(appData, activityConfig); //db, currentActivity, features);
-let snackbar = new SnackBar();
-app.setSnackbar(snackbar);
-if (process.env.NODE_ENV !== "production") {
+let app = new DiInstructionApp(activityData, activityConfig);
+if (process.env.NODE_ENV === "development") {
     app.setStartScene("scene28");
 }
 app.start();
