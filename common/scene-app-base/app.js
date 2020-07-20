f38ca03e4a35bd45bbcf3a34018316cb4d3bd0b4
Scott Silliman <scott.t.silliman@gmail.com>
2020-07-19 20:29:37 -0400

    moved implementation out of index.js. now handles common code from di-prepost/di-instr

---
+++ common/scene-app-base/app.js
@@ -0,0 +1,227 @@
+import { getDBConnection } from "@isptutorproject/isp-database";
+import { SnackBar} from "@isptutorproject/snackbar";
+
+export class SceneBasedApp {
+    constructor(appData, activityConfig, defaultInitialState) {
+        this.sceneData = appData.scenes;
+        this.db = getDBConnection(activityConfig.database);
+        this.db.setCredentials(activityConfig.classCode, activityConfig.userID);
+        this.activityKey = activityConfig.activityKey;
+        this.defaultState = defaultInitialState;
+
+        this.snackbar = new SnackBar();
+
+        this.nextBtn = document.getElementById("next_btn");
+        this.prevBtn = document.getElementById("prev_btn");
+        this.sceneIdRegion = document.getElementById("scene_id_region");
+
+        this.handleNext = this.handleNext.bind(this);
+        this.handlePrev = this.handlePrev.bind(this);
+        this.nextBtn.addEventListener("click", this.handleNext);
+        this.prevBtn.addEventListener("click", this.handlePrev);
+
+        this.state = {};
+        this.scenes = {};
+        this.startScene = "start";
+        this.currentScene = null;
+        this.prevScene = null;
+        // is this needed???
+        this.bogusSceneInfo = {
+            id: "N/A",
+            sceneType: "bogus"
+        };
+        window.app = this;
+    }
+
+
+    // setSnackbar(snackbar) {
+    //     this.snackbar = snackbar;
+    // }
+
+    showFeedback(feedback) {
+        this.snackbar.show(feedback);
+    }
+
+    handlePrev(event) {
+        event.preventDefault();
+        this.currentScene.handlePrevButton();
+        this.saveAppState();
+    }
+
+    handleNext(event) {
+        event.preventDefault();
+        this.currentScene.handleNextButton();
+        this.saveAppState();
+    }
+
+    setStartScene(sceneId) {
+        console.debug("setStartScene()", sceneId);
+        this.startScene = sceneId;
+    }
+
+    start() {
+        console.log("starting");
+        this.getAppState()
+            .then((state) => {
+                if (null === state) {
+                    console.log("no activity data in db. using initial data")
+                    state = this.defaultState;
+                }
+                this.state = state;
+                return;
+            })
+            .then(() => {
+                console.log("Initializing Scenes");
+                for (let section of document.querySelectorAll("section.scene")) {
+                    let sceneId = section.id;
+                    // console.log(`creating scene for ${sceneId}`);
+                    let sceneObj = this.createScene(this.sceneData[sceneId]);
+                    // console.log(sceneObj);
+                    this.scenes[sceneId] = sceneObj;
+                }
+                return;
+            })
+            .then(() => {
+                console.log(this.state);
+                console.log(this.state.sceneState);
+                console.log("Restoring App State")
+                for (let section of document.querySelectorAll("section.scene")) {
+                    let sceneId = section.id;
+                    let sceneState = this.state.sceneState[sceneId];
+                    this.scenes[sceneId].restoreState(sceneState);
+                }
+                if (this.state.currentScene) {
+                    console.log("Restoring Current Scene");
+                    this.currentScene = this.lookupScene(this.state.currentScene);
+                }
+                return;
+            })
+            .then(() => {
+                // console.debug(this.sceneData);
+                // console.log(this.scenes);
+            })
+            .then(() => {
+                this.gotoStartScene();
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
+
+    getAppState() {
+        console.debug("Loading App State");
+        return this.db.getActivityData(this.activityKey);
+    }
+
+    saveAppState() {
+        console.debug("Saving App State");
+        return this.db.setActivityData(this.activityKey, this.state);
+    }
+
+    createScene(sceneInfo) {
+        // you'll want to override this to do anything useful
+        // console.debug("createScene()", sceneInfo, sceneState);
+        return new Scene(this, sceneInfo);
+    }
+
+    switchToScene(scene) {
+        this.logSceneChange(scene);
+        this.gotoScene(scene);
+    }
+
+    gotoScene(scene) {
+        this.prevScene = this.currentScene;
+        if (this.prevScene) {
+            this.prevScene.pre_exit();
+            this.prevScene.exit();
+            this.prevScene.post_exit();
+        }
+        this.currentScene = scene;
+        this.currentScene.pre_enter();
+        this.currentScene.enter();
+        this.currentScene.post_enter();
+        // delete this.prevScene;
+        this.displaySceneId();
+    }
+
+    displaySceneId() {
+        if (process.env.NODE_ENV === "development") {
+            this.sceneIdRegion.innerHTML = this.currentScene.id;
+        }
+    }
+
+    lookupScene(sceneId) {
+        let scene = this.scenes[sceneId];
+        if (!scene) {
+            console.error(`ERROR: no such scene ${sceneId}`);
+        }
+        return scene;
+    }
+
+    followEdge(edgeName) {
+        let newSceneId = this.currentScene.edges[edgeName];
+        let newScene = this.lookupScene(newSceneId);
+        this.switchToScene(newScene);
+    }
+
+    logSceneChange(scene) {
+        let data = {
+            action: "ENTER_SCENE",
+            from: this.currentScene.id,
+            to: scene.id,
+            timestamp: Date.now()
+        };
+        this.state.events.push(data);
+        console.debug("Updating state.events");
+    }
+
+
+    gotoStartScene() {
+        // bypasses switchToScene() and thus logSceneChange()
+        // this.currentScene = null;
+        let startScene = this.currentScene;
+        if (!startScene) {
+            startScene = this.lookupScene(this.startScene);
+        }
+        this.gotoScene(startScene);
+    }
+
+
+    hide(el) {
+        if (el) { el.classList.add("hidden"); }
+    }
+
+    show(el) {
+        if (el) { el.classList.remove("hidden"); }
+    }
+
+    makeInvisible(el) {
+        if (el) {
+            el.classList.add("invisible");
+        }
+    }
+
+    makeVisible(el) {
+        if (el) {
+            el.classList.remove("invisible");
+        }
+    }
+
+    disable(el) {
+        if (el) {
+            // console.log("disabling:", el);
+            el.classList.add("disabled");
+        } else {
+            // console.log("not disabling non-existinant element:", el);
+        }
+    }
+
+    enable(el) {
+        if (el) {
+            // console.log("disabling:", el);
+            el.classList.remove("disabled");
+        } else {
+            // console.log("not disabling non-existinant element:", el);
+        }
+    }
+}
