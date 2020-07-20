0f61c66de611b2400bbc305ad18fddf69daa3db8
Scott Silliman <scott.t.silliman@gmail.com>
2020-07-19 20:30:24 -0400

    moved out of index.js  now handles common code from di-instr/di-prepost

---
+++ common/scene-app-base/scene.js
@@ -0,0 +1,132 @@
+export class Scene {
+    constructor(app, sceneInfo) {
+        console.debug(`\tInitializing Scene: ${sceneInfo.id}`);
+        this.app = app;
+        // copy all sceneInfo to 'this'
+        for (let [k, v] of Object.entries(sceneInfo)) {
+            this[k] = v;
+        }
+        this.el = document.getElementById(this.id);
+        // bind event handler
+        this.handleFollowEdgeButton = this.handleFollowEdgeButton.bind(this);
+    }
+
+    showBtns(btnNames) {
+        for (let btnName of btnNames) {
+            if (this.btnNames.hasOwnProperty(btnName)) {
+                this.app.show(this.btnNames[btnName]);
+            }
+        }
+    }
+
+    hideBtns(btnNames) {
+        for (let btnName of btnNames) {
+            if (this.btnNames.hasOwnProperty(btnName)) {
+                this.app.hide(this.btnNames[btnName]);
+            }
+        }
+    }
+
+
+    handleFollowEdgeButton(e) {
+        e.preventDefault();
+        let edgeName = e.target.dataset.edge;
+        this.app.followEdge(edgeName);
+    }
+
+    getFollowEdgeElements() {
+        return this.el.getElementsByClassName("follow-edge");
+    }
+
+    setupFollowEdgeEventHandlers() {
+        for (let el of this.getFollowEdgeElements()) {
+            el.addEventListener(
+                "click", this.handleFollowEdgeButton, { once: true }
+            );
+        }
+    }
+
+    restoreState(value) {
+        if (typeof (value) !== "undefined") {
+            console.debug(`\tRestoring Scene State for: ${this.id} `);
+            return true;
+        }
+        return false;
+    }
+
+    _isBogus() {
+        return this.id === "N/A" || this.sceneType === "bogus";
+    }
+
+    handlePrevButton() {
+        this.app.followEdge("prev");
+    }
+
+    handleNextButton() {
+        this.app.followEdge("next");
+    }
+
+    hasForm() {
+        return false;
+    }
+
+    formChanged() {
+        return false;
+    }
+
+    pre_enter() {
+
+    }
+    enter() {
+        if (this._isBogus()) {
+            return;
+        }
+        console.debug(`Entering Scene: ${this.id}`);
+        console.debug("\tUpdating state.currentScene");
+        this.app.state.currentScene = this.id;
+        this.defaultEnterSceneActions();
+        if (this.customEnterActions.length) {
+            this.performCustomEnterSceneActions();
+        }
+    }
+
+    post_enter() {
+
+    }
+
+    pre_exit() {
+
+    }
+
+    exit() {
+        if (this._isBogus()) {
+            return;
+        }
+        console.debug(`Exiting Scene: ${this.id}`);
+        this.defaultExitSceneActions();
+        if (this.customExitActions.length) {
+            this.performCustomExitSceneActions();
+        }
+    }
+
+    post_exit() {
+
+    }
+
+    defaultEnterSceneActions() {
+        this.el.classList.add("active");
+        this.setupFollowEdgeEventHandlers();
+    }
+
+    defaultExitSceneActions() {
+        this.el.classList.remove("active");
+    }
+
+    performCustomEnterSceneActions() {
+        console.warn("Unimplemented Method: performCustomEnterSceneActions");
+    }
+
+    performCustomExitSceneActions() {
+        console.warn("Unimplemented Method: performCustomExitSceneActions");
+    }
+}
