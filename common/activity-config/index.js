3bc4a75789a207a06abf5cb6bad3c1282fb393b4
Scott Silliman <scott.t.silliman@gmail.com>
2020-07-19 20:57:29 -0400

    added 'homepage' url to struct

--- common/activity-config/index.js
+++ common/activity-config/index.js
@@ -3,6 +3,7 @@ export function getActivityConfiguration() {
         userID: localStorage.getItem("userID"),
         classCode: localStorage.getItem("classCode"),
         database: localStorage.getItem("database"),
+        homepage: localStorage.getItem("homepage"),
         activityKey: localStorage.getItem("currentActivity"),
         features: (localStorage.getItem("currentActivityFeatures") || "")
                   .split(":")
