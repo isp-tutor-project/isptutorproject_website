c6f09d44001060f649f08036c633c77726ddc41f
Scott Silliman <scott.t.silliman@gmail.com>
2020-07-19 20:41:13 -0400

    renamed 'transitions' to 'edges'

--- apps/di-prepost/data/diPreTestData.js
+++ apps/di-prepost/data/diPreTestData.js
@@ -57,222 +57,224 @@ module.exports = {
       }
     }
   },
-            transitions: {
-                next: "intro"
-            }, 
-            customEnterActions: [
-                {
-                    name: "hideBtns",
-                    args: ["prev", "next"]
-                }
-            ]
-        },
-        intro: {
-            transitions: {
-                prev: "start",
-                next: "memoryIntro1"
-            }
-        },
-        memoryIntro1: {
-            transitions: {
-                prev: "intro",
-                next: "memoryIntro2"
-            }
-        },
-        memoryIntro2: {
-            transitions: {
-                prev: "memoryIntro1",
-                next: "memoryIntro3"
-            }
-        },
-        memoryIntro3: {
-            transitions: {
-                prev: "memoryIntro2",
-                next: "memoryIntro4"
-            }
-        },
-        memoryIntro4: {
-            transitions: {
-                prev: "memoryIntro3",
-                next: "memoryIntro5"
-            }
-        },
-        memoryIntro5: {
-            transitions: {
-                prev: "memoryIntro4",
-                next: "memoryIntro6"
-            }
-        },
-        memoryIntro6: {
-            transitions: {
-                prev: "memoryIntro5",
-                next: "memoryIntro7"
-            }
-        },
-        memoryIntro7: {
-            transitions: {
-                prev: "memoryIntro6",
-                next: "memoryResults1Intro"
-            }
-        },
-        memoryResults1Intro: {
-            transitions: {
-                prev: "memoryIntro7",
-                next: "memoryResults1"
-            }
-        },
-        memoryResults1: {
-            sceneType: "memoryResults",
-            transitions: {
-                prev: "memoryResults1Intro",
-                next: "memoryResults2Intro"
-            }
-        },
-        memoryResults2Intro: {
-            transitions: {
-                prev: "memoryResults1",
-                next: "memoryResults2"
-            }
-        },
-        memoryResults2: {
-            sceneType: "memoryResults",
-            transitions: {
-                prev: "memoryResults2Intro",
-                next: "memoryResults3Intro"
-            }
-        },
-        memoryResults3Intro: {
-            transitions: {
-                prev: "memoryResults2",
-                next: "memoryResults3"
-            }
-        },
-        memoryResults3: {
-            sceneType: "memoryResults",
-            transitions: {
-                prev: "memoryResults3Intro",
-                next: "memoryResults4Intro"
-            }
-        },
-        memoryResults4Intro: {
-            transitions: {
-                prev: "memoryResults3",
-                next: "memoryResults4"
-            }
-        },
-        memoryResults4: {
-            sceneType: "memoryResults",
-            transitions: {
-                prev: "memoryResults4Intro",
-                next: "memoryResults5Intro"
-            }
-        },
-        memoryResults5Intro: {
-            transitions: {
-                prev: "memoryResults4",
-                next: "memoryResults5"
-            }
-        },
-        memoryResults5: {
-            sceneType: "memoryResults",
-            transitions: {
-                prev: "memoryResults5Intro",
-                next: "rocketsIntro1"
-            }
-        },
-        rocketsIntro1: {
-            transitions: {
-                prev: "memoryResults5",
-                next: "rocketsIntro2"
-            }
-        },
-        rocketsIntro2: {
-            transitions: {
-                prev: "rocketsIntro1",
-                next: "rocketsIntro3"
-            }
-        },
-        rocketsIntro3: {
-            transitions: {
-                prev: "rocketsIntro2",
-                next: "rocketsIntro4"
-            }
-        },
-        rocketsIntro4: {
-            transitions: {
-                prev: "rocketsIntro3",
-                next: "rocketsResults1Intro"
-            }
-        },
-        rocketsResults1Intro: {
-            transitions: {
-                prev: "rocketsIntro4",
-                next: "rocketsResults1"
-            }
-        },
-        rocketsResults1: {
-            sceneType: "rocketsResults",
-            transitions: {
-                prev: "rocketsResults1Intro",
-                next: "rocketsResults2Intro"
-            }
-        },
-        rocketsResults2Intro: {
-            transitions: {
-                prev: "rocketsResults1",
-                next: "rocketsResults2"
-            }
-        },
-        rocketsResults2: {
-            sceneType: "rocketsResults",
-            transitions: {
-                prev: "rocketsResults2Intro",
-                next: "rocketsResults3Intro"
-            }
-        },
-        rocketsResults3Intro: {
-            transitions: {
-                prev: "rocketsResults2",
-                next: "rocketsResults3"
-            }
-        },
-        rocketsResults3: {
-            sceneType: "rocketsResults",
-            transitions: {
-                prev: "rocketsResults3Intro",
-                next: "rocketsResults4Intro"
-            }
-        },
-        rocketsResults4Intro: {
-            transitions: {
-                prev: "rocketsResults3",
-                next: "rocketsResults4"
-            }
-        },
-        rocketsResults4: {
-            sceneType: "rocketsResults",
-            transitions: {
-                prev: "rocketsResults4Intro",
-                next: "rocketsResults5Intro"
-            }
-        },
-        rocketsResults5Intro: {
-            transitions: {
-                prev: "rocketsResults4",
-                next: "rocketsResults5"
-            }
-        },
-        rocketsResults5: {
-            sceneType: "rocketsResults",
-            transitions: {
-                prev: "rocketsResults5Intro",
-                next: "completed"
-            }
-        },
-        completed: {
-            transitions: {
-                prev: "rocketsResults5"
-            }
+  scenes: {
+    start: {
+      edges: {
+        next: "intro"
+      },
+      customEnterActions: [
+        {
+          name: "hideBtns",
+          args: ["prev", "next"]
         }
+      ]
+    },
+    intro: {
+      edges: {
+        prev: "start",
+        next: "memoryIntro1"
+      }
+    },
+    memoryIntro1: {
+      edges: {
+        prev: "intro",
+        next: "memoryIntro2"
+      }
+    },
+    memoryIntro2: {
+      edges: {
+        prev: "memoryIntro1",
+        next: "memoryIntro3"
+      }
+    },
+    memoryIntro3: {
+      edges: {
+        prev: "memoryIntro2",
+        next: "memoryIntro4"
+      }
+    },
+    memoryIntro4: {
+      edges: {
+        prev: "memoryIntro3",
+        next: "memoryIntro5"
+      }
+    },
+    memoryIntro5: {
+      edges: {
+        prev: "memoryIntro4",
+        next: "memoryIntro6"
+      }
+    },
+    memoryIntro6: {
+      edges: {
+        prev: "memoryIntro5",
+        next: "memoryIntro7"
+      }
+    },
+    memoryIntro7: {
+      edges: {
+        prev: "memoryIntro6",
+        next: "memoryResults1Intro"
+      }
+    },
+    memoryResults1Intro: {
+      edges: {
+        prev: "memoryIntro7",
+        next: "memoryResults1"
+      }
+    },
+    memoryResults1: {
+      sceneType: "memoryResults",
+      edges: {
+        prev: "memoryResults1Intro",
+        next: "memoryResults2Intro"
+      }
+    },
+    memoryResults2Intro: {
+      edges: {
+        prev: "memoryResults1",
+        next: "memoryResults2"
+      }
+    },
+    memoryResults2: {
+      sceneType: "memoryResults",
+      edges: {
+        prev: "memoryResults2Intro",
+        next: "memoryResults3Intro"
+      }
+    },
+    memoryResults3Intro: {
+      edges: {
+        prev: "memoryResults2",
+        next: "memoryResults3"
+      }
+    },
+    memoryResults3: {
+      sceneType: "memoryResults",
+      edges: {
+        prev: "memoryResults3Intro",
+        next: "memoryResults4Intro"
+      }
+    },
+    memoryResults4Intro: {
+      edges: {
+        prev: "memoryResults3",
+        next: "memoryResults4"
+      }
+    },
+    memoryResults4: {
+      sceneType: "memoryResults",
+      edges: {
+        prev: "memoryResults4Intro",
+        next: "memoryResults5Intro"
+      }
+    },
+    memoryResults5Intro: {
+      edges: {
+        prev: "memoryResults4",
+        next: "memoryResults5"
+      }
+    },
+    memoryResults5: {
+      sceneType: "memoryResults",
+      edges: {
+        prev: "memoryResults5Intro",
+        next: "rocketsIntro1"
+      }
+    },
+    rocketsIntro1: {
+      edges: {
+        prev: "memoryResults5",
+        next: "rocketsIntro2"
+      }
+    },
+    rocketsIntro2: {
+      edges: {
+        prev: "rocketsIntro1",
+        next: "rocketsIntro3"
+      }
+    },
+    rocketsIntro3: {
+      edges: {
+        prev: "rocketsIntro2",
+        next: "rocketsIntro4"
+      }
+    },
+    rocketsIntro4: {
+      edges: {
+        prev: "rocketsIntro3",
+        next: "rocketsResults1Intro"
+      }
+    },
+    rocketsResults1Intro: {
+      edges: {
+        prev: "rocketsIntro4",
+        next: "rocketsResults1"
+      }
+    },
+    rocketsResults1: {
+      sceneType: "rocketsResults",
+      edges: {
+        prev: "rocketsResults1Intro",
+        next: "rocketsResults2Intro"
+      }
+    },
+    rocketsResults2Intro: {
+      edges: {
+        prev: "rocketsResults1",
+        next: "rocketsResults2"
+      }
+    },
+    rocketsResults2: {
+      sceneType: "rocketsResults",
+      edges: {
+        prev: "rocketsResults2Intro",
+        next: "rocketsResults3Intro"
+      }
+    },
+    rocketsResults3Intro: {
+      edges: {
+        prev: "rocketsResults2",
+        next: "rocketsResults3"
+      }
+    },
+    rocketsResults3: {
+      sceneType: "rocketsResults",
+      edges: {
+        prev: "rocketsResults3Intro",
+        next: "rocketsResults4Intro"
+      }
+    },
+    rocketsResults4Intro: {
+      edges: {
+        prev: "rocketsResults3",
+        next: "rocketsResults4"
+      }
+    },
+    rocketsResults4: {
+      sceneType: "rocketsResults",
+      edges: {
+        prev: "rocketsResults4Intro",
+        next: "rocketsResults5Intro"
+      }
+    },
+    rocketsResults5Intro: {
+      edges: {
+        prev: "rocketsResults4",
+        next: "rocketsResults5"
+      }
+    },
+    rocketsResults5: {
+      sceneType: "rocketsResults",
+      edges: {
+        prev: "rocketsResults5Intro",
+        next: "completed"
+      }
+    },
+    completed: {
+      edges: {
+        prev: "rocketsResults5"
+      }
     }
+  }
 };
