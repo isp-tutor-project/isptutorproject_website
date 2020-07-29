85eb17b4d28f67540eb72ac416537abd579e72c3
Scott Silliman <scott.t.silliman@gmail.com>
2020-07-29 14:44:45 -0400

    removed kruft; differentiates between single/dual table result scenes

--- apps/di-prepost/src/index.js
+++ apps/di-prepost/src/index.js
@@ -4,21 +4,24 @@ import "./index.scss";
 import { NavBar } from "@isptutorproject/navbar";
 
 import { SceneBasedApp } from "@isptutorproject/scene-app-base";
-import { DiTestScene, DiTestResultsScene } from "./scenes";
+import {
+    DiTestScene,
+    DiDualTableResultsScene,
+    DiSingleTableResultsScene
+} from "./scenes";
 
 export class DiTestApp extends SceneBasedApp{
-    // constructor(appData, activityConfig, defaultInitialState) {
-    //     super(appData, activityConfig, defaultInitialState);
-    // }
 
     createScene(sceneInfo) {
         let scene;
         switch(sceneInfo.sceneType) {
             case "carResults":
+            case "rocketsResults":
+                scene = new DiSingleTableResultsScene(this, sceneInfo);
+                break;
             case "libraryResults":
             case "memoryResults":
-            case "rocketsResults":
-                scene = new DiTestResultsScene(this, sceneInfo);
+                scene = new DiDualTableResultsScene(this, sceneInfo);
                 break;
             default:
                 scene = new DiTestScene(this, sceneInfo);
@@ -27,25 +30,5 @@ export class DiTestApp extends SceneBasedApp{
         return scene;
     }
 
-    // logStudentAnswer(questionName, answer) {
-    //     let question = this.currentScene.questions[questionName];
-    //     let isCorrect = "N/A";
-    //     if (question.hasOwnProperty("correctAnswer")) {
-    //         isCorrect = (answer === question.correctAnswer)
-    //     }
-    //     let qid = `${this.currentScene.id}.${questionName}`;
-    //     if ("" !== this.testName) {
-    //         qid = `${this.testName}.${qid}`;
-    //     }
-    //     let data = {
-    //         action: "SUBMIT_ANSWER",
-    //         questionId: qid,
-    //         questionText: question.text,
-    //         answer: answer,
-    //         isCorrect: isCorrect,
-    //         timestamp: new Date().toLocaleString()
-    //     }
-    //     console.log(data);
-    // }
 
 }
