1a4bf7498a088199f5ff81e824cfc903571133bd
Scott Silliman <scott.t.silliman@gmail.com>
2020-08-08 18:14:39 -0400

    created new data builder for DiCrystal

--- apps/di-prepost/scripts/buildData.js
+++ apps/di-prepost/scripts/buildData.js
@@ -90,9 +90,51 @@ class DiPostTestDataBuilder extends DataBuilder {
     }
 }
 
+class DiCrystalDataBuilder extends DataBuilder {
+
+    mungeData(data) {
+        let newData = { scenes: {} };
+        let questions = data.questions;
+        let errorsFound = false;
+        for (let [sceneId, sceneData] of Object.entries(data.scenes)) {
+            sceneData['id'] = sceneId;
+            for (let [edgeName, edgeSceneId] of Object.entries(sceneData.edges)) {
+                if (!VALID_EDGE_NAMES.includes(edgeName)) {
+                    console.error(`ERROR: ${sceneId} has an invalid edge: ${edgeName}`);
+                    errorsFound = true;
+                }
+                if (!data.scenes.hasOwnProperty(edgeSceneId)) {
+                    console.error(`ERROR: ${sceneId}.edges.${edgeName} points to an invalid scene: ${edgeSceneId}`);
+                    errorsFound = true;
+                }
+            }
+            sceneData = addSceneTypeIfMissing(sceneData);
+
+            if ("crystalResults" === sceneData.sceneType) {
+                sceneData.questions = questions.crystal;
+            }
+            if (sceneData.hasOwnProperty("correctQ1Answer")) {
+                sceneData.questions.q1.correctAnswer = sceneData.correctQ1Answer;
+                delete sceneData.correctA1Answer;
+            }
+
+            sceneData = addCustomEnterActionsIfMissing(sceneData);
+            sceneData = addCustomExitActionsIfMissing(sceneData);
+            newData.scenes[sceneId] = sceneData;
+        }
+        if (errorsFound) {
+            newData = null;
+        }
+        return newData;
+    }
+}
+
 
 let builder = new DiPreTestDataBuilder("diPreTest");
 builder.buildData();
 
 let builder2 = new DiPostTestDataBuilder("diPostTest");
 builder2.buildData();
+
+let builder3 = new DiCrystalDataBuilder("diCrystalTest");
+builder3.buildData();
