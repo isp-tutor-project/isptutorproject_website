7f83fa86763cc2d81b0d9f76fa0ab5c3489f7be4
Scott Silliman <scott.t.silliman@gmail.com>
2020-08-08 18:12:14 -0400

    initial commit

---
+++ apps/di-prepost/src/crystal.js
@@ -0,0 +1,31 @@
+import { DiTestApp } from "./index";
+import { NavBar } from "@isptutorproject/navbar";
+
+import { getActivityConfiguration } from "@isptutorproject/activity-config";
+
+
+const DEFAULT_APP_INITIAL_STATE = {
+    events: [],
+    // mapping of sceneIDs -> scene-type specific data
+    sceneState: {},
+    currentScene: "start"
+};
+
+
+let activityConfig = getActivityConfiguration();
+const activityData = require("../data/diCrystalTest.json");
+
+let navbar = new NavBar();
+navbar.displayActivityTitle("Crystal Growth Data Interpretation");
+navbar.displayUser(activityConfig.userID);
+
+// console.log(activityConfig);
+
+let app = new DiTestApp(
+    activityData, activityConfig, DEFAULT_APP_INITIAL_STATE
+);
+if (process.env.NODE_ENV === "development") {
+    app.setStartScene("start");
+}
+app.start()
+
