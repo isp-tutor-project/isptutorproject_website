7a39ff7288f806d4f20118d09ef19040fc59d0bf
Scott Silliman <scott.t.silliman@gmail.com>
2020-12-03 14:23:05 -0500

    removed kruft

--- scripts/getMetaData.py
+++ scripts/getMetaData.py
@@ -5,19 +5,12 @@ import json
 import os
 import sys
 
-from box import Box
-from box.exceptions import BoxValueError
-# import firebase_admin
-# from firebase_admin import firestore
 import xlsxwriter
 
 from shared import (
     DEVEL_CLASS_CODES, META_DATA_FLDS, JSON_FLDS, get_file_path
 )
 
-# default_app = firebase_admin.initialize_app()
-# db = firestore.client()
-# snapshot = db.collection("STUDY_3")
 
 data_path = get_file_path()
 if not os.path.exists(data_path):
@@ -42,43 +35,5 @@ workbook = xlsxwriter.Workbook(spread_sheet)
 worksheet = workbook.add_worksheet()
 for row_num, row in enumerate(rows):
     for col_num, value in enumerate(row):
-        # print(row_num, col_num, value)
         worksheet.write(row_num, col_num, value)
 workbook.close()
-
-# ALL_USERS = []
-# for doc in snapshot.get():
-#     if "study_data" == doc.id:
-#         continue
-#     data = doc.to_dict()
-#     class_code = data.get("classCode", None)
-#     if class_code is None or class_code in DEVEL_CLASS_CODES:
-#         continue
-#     userID = data.get("userID")
-#     if "matsPre" not in data:
-#         continue
-#     matsPre = data["matsPre"]
-#     matsPre = json.loads(matsPre)
-#     if isinstance(matsPre, str):
-#         matsPre = json.loads(matsPre)
-#     matsPre = Box(matsPre)
-
-#     row = [
-#         userID,
-#         class_code,
-#         data.get("FN"),
-#         data.get("LN"),
-#         data.get("MON"),
-#         data.get("DAY"),
-#         data.get("completedAssignments")
-#     ]
-#     ALL_USERS.append(row)
-
-# workbook = xlsxwriter.Workbook("all_user_metadata.xlsx")
-# worksheet = workbook.add_worksheet()
-# for row_num, row in enumerate(ALL_USERS):
-#     for col_num, value in enumerate(row):
-#         # print(row_num, col_num, value)
-#         worksheet.write(row_num, col_num, value)
-# workbook.close()
-
