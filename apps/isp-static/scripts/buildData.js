7fcc8adb462eaa12dc2bcd3b5abe8e919ba06ff2
Scott Silliman <scott.t.silliman@gmail.com>
2020-12-18 16:24:37 -0500

    now master buildData script for all apps

--- apps/isp-static/scripts/buildData.js
+++ apps/isp-static/scripts/buildData.js
@@ -1,4 +1,26 @@
 const { DataBuilder } = require("@isptutorproject/isp-data");
+const {
+    DiPreTestDataBuilder,
+    DiPostTestDataBuilder,
+    DiCrystalDataBuilder
+} = require("./di-prepost");
 
-let builder = new DataBuilder("diInstr");
-builder.buildData()
+const { RQIntroDataBuilder } = require("./rq-intro");
+
+
+let diInstr, diPre, diPost, diCrystal, rqIntro;
+
+diInstr = new DataBuilder("diInstr");
+diInstr.buildData()
+
+diPre = new DiPreTestDataBuilder("diPreTest");
+diPre.buildData();
+
+diPost = new DiPostTestDataBuilder("diPostTest");
+diPost.buildData();
+
+diCrystal = new DiCrystalDataBuilder("diCrystalTest");
+diCrystal.buildData();
+
+rqIntro = new RQIntroDataBuilder("rqIntro");
+rqIntro.buildData();
