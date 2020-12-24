f1a1dfd0628cde17f018b73cc56ded35f7294b10
Scott Silliman <scott.t.silliman@gmail.com>
2020-12-24 10:30:20 -0500

    latest WIP

--- apps/isp-static/src/hypo-gr/index.js
+++ apps/isp-static/src/hypo-gr/index.js
@@ -24,6 +24,7 @@ const DEFAULT_APP_INITIAL_STATE = {
     currentScene: "start"
 };
 
+const NO_TRANSITIONS = -Infinity;
 
 class HypoGRScene extends Scene {
     constructor(app, sceneInfo) {
@@ -36,6 +37,27 @@ class HypoGRScene extends Scene {
             prev: this.app.prevBtn,
             next: this.app.nextBtn
         };
+        this.currentTransition = 0;
+        // console.log(sceneInfo);
+
+        this.el = document.getElementById(sceneInfo.id);
+        this.showOnTransitionsGte = new Map();
+        this.maxTransitions = NO_TRANSITIONS;
+        for (let child of this.el.querySelectorAll(".show-on-transition")){
+            // child.classList.add("invisible");
+            if (child.dataset && child.dataset.whenTransitionGte) {
+                let val = parseInt(child.dataset.whenTransitionGte, 10);
+                if ((val) > this.maxTransitions) {
+                    this.maxTransitions = val;
+                }
+                if (! this.showOnTransitionsGte.has(val)) {
+                    this.showOnTransitionsGte.set(val, []);
+                }
+                this.showOnTransitionsGte.get(val).push(child);
+            }
+        }
+        console.log(this.showOnTransitionsGte);
+        console.log(this.maxTransitions);
     }
 
     performCustomEnterSceneActions() {
@@ -46,6 +68,53 @@ class HypoGRScene extends Scene {
             if (this.customActions.hasOwnProperty(name)) {
                 this[name](args);
             }
+        }
+    }
+
+    defaultEnterSceneActions() {
+        super.defaultEnterSceneActions();
+        this.app.show(this.app.prevBtn);
+        this.app.enable(this.app.prevBtn);
+        this.app.show(this.app.nextBtn);
+        this.app.enable(this.app.nextBtn);
+        if (NO_TRANSITIONS !== this.maxTransitions) {
+            this.handleTransitions();
+        }
+    }
+
+    handleTransitions() {
+        for (let i=this.maxTransitions; i>0; i--) {
+            for (let e of this.showOnTransitionsGte.get(i)) {
+                if (this.currentTransition >= i) {
+                    e.classList.remove("invisible");
+                } else {
+                    e.classList.add("invisible");
+                }
+            }
+        }
+    }
+
+    handleNextButton() {
+        if ((NO_TRANSITIONS === this.maxTransitions) ||
+            (this.currentTransition === this.maxTransitions)) {
+            console.log(`${this.currentTransition} > ${this.maxTransitions}`);
+            console.log("greater than");
+            super.handleNextButton();
+        }  else {
+            this.currentTransition++;
+            this.handleTransitions();
+            console.log(`${this.currentTransition} <= ${this.maxTransitions}`);
+            console.log("not greater than");
+        }
+    }
+
+    handlePrevButton() {
+        console.log(`${this.currentTransition}`);
+        if (NO_TRANSITIONS === this.maxTransitions || 0 === this.currentTransition) {
+            super.handlePrevButton();
+        } else {
+            this.currentTransitions--;
+            this.handleTransitions();
         }
     }
 }
