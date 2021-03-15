62ffd056beb4230fcb847a8dfef7fc85231dd925
Scott Silliman <scott.t.silliman@gmail.com>
2021-03-15 08:07:21 -0400

    shared scene data

--- apps/isp-static/data/exp-design/expDesignSharedData.js
+++ apps/isp-static/data/exp-design/expDesignSharedData.js
@@ -1,12 +1,60 @@
-const { ExpDesignDataBuilder } = require("./builder");
 
-const expDesignData = {
-    scenes: {
-        intro: {
-            edges: []
+const expDesignSharedScenes = {
+    intro10: {
+        edges: {
+            next: "intro20"
+        }
+    },
+    intro20: {
+        edges: {
+            prev: "intro10",
+            next: "intro30"
+        }
+    },
+    intro30: {
+        edges: {
+            prev: "intro20",
+            next: "intro-select-IV"
+        }
+    },
+    "intro-select-IV": {
+        edges: {
+            prev: "intro30",
+            next: "intro-select-IV-values"
+        }
+    },
+    "intro-select-IV-values": {
+        edges: {
+            prev: "intro-select-IV",
+            next: "intro-think-about-other-variables"
+        }
+    },
+    "intro-think-about-other-variables": {
+        edges: {
+            prev: "intro-select-IV-values",
+            next: "intro-select-var1-values"
+        }
+    },
+    "intro-select-var1-values": {
+        edges: {
+            prev: "intro-think-about-other-variables",
+            next: "intro-select-var2-values"
+        }
+    },
+    "intro-select-var2-values": {
+        edges: {
+            prev: "intro-select-var1-values",
+            next: "intro-select-var3-values"
+        }
+    },
+    "intro-select-var3-values": {
+        edges: {
+            prev: "intro-select-var2-values",
+            next: "intro-think-about-procedure"
         }
     }
-};
+}
 
-let bldr = new ExpDesignDataBuilder(expDesignData);
-module.exports = bldr.buildData();
+module.exports = {
+    expDesignSharedScenes
+}
