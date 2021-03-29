bfda6eb2ca3b6f30c4dfcb7fc6954bf5953d58a8
Scott Silliman <scott.t.silliman@gmail.com>
2021-03-29 16:20:37 -0400

    added special handling/checking of next links for ynNavigationScenes

--- apps/isp-static/data/sf-assessment/builders.js
+++ apps/isp-static/data/sf-assessment/builders.js
@@ -14,18 +14,34 @@ class ScienceFairAssessmentDataBuilder extends DataBuilder {
         let errorsFound = false;
         for (let [sceneId, sceneData] of Object.entries(data.scenes)) {
             sceneData['id'] = sceneId;
-            for (let [edgeName, edgeSceneId] of Object.entries(sceneData.edges)) {
-                if (!VALID_EDGE_NAMES.includes(edgeName)) {
-                    console.error(`ERROR: ${sceneId} has an invalid edge: ${edgeName}`);
-                    errorsFound = true;
+            if (!!sceneData.sceneType && "ynNavigationScene" === sceneData.sceneType) {
+                const prevSceneId = sceneData.edges.prev
+                const nextSceneIds = sceneData.edges.next
+                if (!data.scenes.hasOwnProperty(prevSceneId)) {
+                    console.error(`ERROR: ${sceneId} has invalid prev scene ${prevSceneId}`)
+                    errorsFound = true
                 }
-                if (!data.scenes.hasOwnProperty(edgeSceneId)) {
-                    console.error(`ERROR: ${sceneId}.edges.${edgeName} points to an invalid scene: ${edgeSceneId}`);
-                    errorsFound = true;
+                if (!data.scenes.hasOwnProperty(nextSceneIds.y)) {
+                    console.error(`ERROR: ${sceneId} has invalid next.y scene ${nextSceneIds.y}`)
+                    errorsFound = true
                 }
+                if (!data.scenes.hasOwnProperty(nextSceneIds.n)) {
+                    console.error(`ERROR: ${sceneId} has invalid next.n scene ${nextSceneIds.n}`)
+                    errorsFound = true
+                }
+            } else {
+                for (let [edgeName, edgeSceneId] of Object.entries(sceneData.edges)) {
+                    if (!VALID_EDGE_NAMES.includes(edgeName)) {
+                        console.error(`ERROR: ${sceneId} has an invalid edge: ${edgeName}`);
+                        errorsFound = true;
+                    }
+                    if (!data.scenes.hasOwnProperty(edgeSceneId)) {
+                        console.error(`ERROR: ${sceneId}.edges.${edgeName} points to an invalid scene: ${edgeSceneId}`);
+                        errorsFound = true;
+                    }
+                }
+                sceneData = addSceneTypeIfMissing(sceneData);
             }
-            sceneData = addSceneTypeIfMissing(sceneData);
-
             sceneData = addCustomEnterActionsIfMissing(sceneData);
             sceneData = addCustomExitActionsIfMissing(sceneData);
             newData.scenes[sceneId] = sceneData;
